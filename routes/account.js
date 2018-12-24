f6763eea643e5bcdc9e83185381db31418fa7b29
fullstack360 <info@fullstack360.com>
2018-12-24 17:17:09 -0500

    use non ssl for now

--- routes/account.js
+++ routes/account.js
@@ -428,7 +428,8 @@ router.post('/:action', function(req, res, next){
 
 			// send POST request to https://platform.turbo360-vector.com/launchtemplate
 			// with 'folder' as params
-			const url = 'https://platform.turbo360-vector.com/launchtemplate'
+			// const url = 'https://platform.turbo360-vector.com/launchtemplate'
+			const url = 'http://platform.turbo360-vector.com/launchtemplate'
 			return utils.HTTP.post(url, folder)
 		})
 		.then(data => {
