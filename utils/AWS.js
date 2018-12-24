5129bf87668dce56e8e4afaaaa239a941aa9177d
fullstack360 <info@fullstack360.com>
2018-12-24 02:36:13 -0500

    config

--- utils/AWS.js
+++ utils/AWS.js
@@ -183,7 +183,13 @@ module.exports = {
 				Body: folderName
 			}
 
-			const s3 = s3Client()
+			// const s3 = s3Client()
+			aws.config.update({
+				accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+				secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+			})
+
+			const s3 = new aws.S3()
 			s3.upload(params, function(err, data) {
 				if (err) {
 					reject(err)
