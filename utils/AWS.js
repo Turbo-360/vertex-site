4efc1f1e250d61635ddd81c1b67c2bad1ced7109
fullstack360 <info@fullstack360.com>
2018-12-24 02:04:35 -0500

    misc

--- utils/AWS.js
+++ utils/AWS.js
@@ -14,13 +14,18 @@ const lambdaClient = function(){
 }
 
 const s3Client = function(){
-	aws.config.update({
+	// aws.config.update({
+	// 	region: DEFAULT_ZONE,
+	// 	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	// 	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+	// })
+
+	const s3 = new aws.S3({
 		region: DEFAULT_ZONE,
 		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 	})
 
-	const s3 = new aws.S3()
 	return s3
 }
 
