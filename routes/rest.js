ae2d2fd7f2ccbff67f0f3a3483da2dc5959f9d90
fullstack360 <info@fullstack360.com>
2019-01-09 00:01:38 -0500

    add params and request method args

--- routes/rest.js
+++ routes/rest.js
@@ -11,10 +11,39 @@ const apiBaseUrl = (appSlug, resource, resourceId) => {
   return base+'/'+resource+'/'+resourceId
 }
 
-const queryEndpoint = (endpoint) => {
+const queryEndpoint = (endpoint, params, method) => {
   return new Promise((resolve, reject) => {
-    utils.HTTP.get(endpoint)
-    .then(data => {
+    // utils.HTTP.get(endpoint)
+    // .then(data => {
+    //   try {
+    //     const parsed = JSON.parse(data)
+    //     resolve(parsed)
+    //   }
+    //   catch(err) {
+    //     reject(err)
+    //   }
+    // })
+    // .catch(err => {
+    //   reject(err)
+    // })
+
+
+    // if (method == null)
+    //   method = 'get'
+    //
+    // let request = null
+    // if (method == 'get')
+    //   request = utils.HTTP.get(endpoint, params)
+    // else if (method == 'post')
+    //   request = utils.HTTP.post(endpoint, params)
+    // else if (method == 'put')
+    //   request = utils.HTTP.put(endpoint, params)
+    // else if (method == 'delete')
+    //   request = utils.HTTP.delete(endpoint, params)
+
+    const request = utils.HTTP.get(endpoint)
+
+    request.then(data => {
       try {
         const parsed = JSON.parse(data)
         resolve(parsed)
@@ -26,6 +55,8 @@ const queryEndpoint = (endpoint) => {
     .catch(err => {
       reject(err)
     })
+
+
   })
 }
 
@@ -33,7 +64,7 @@ router.get('/:app/:resource', (req, res, next) => {
 
   // template-test-55-4bkhc9
   const endpoint = apiBaseUrl(req.params.app, req.params.resource)
-  queryEndpoint(endpoint)
+  queryEndpoint(endpoint, null, null)
   .then(data => {
     if (data.confirmation != 'success'){
       throw new Error(data.message)
@@ -52,7 +83,7 @@ router.get('/:app/:resource', (req, res, next) => {
 
 router.get('/:app/:resource/:id', (req, res, next) => {
   const endpoint = apiBaseUrl(req.params.app, req.params.resource, req.params.id)
-  queryEndpoint(endpoint)
+  queryEndpoint(endpoint, null, null)
   .then(data => {
     if (data.confirmation != 'success'){
       throw new Error(data.message)
