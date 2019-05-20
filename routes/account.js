ea3900def11e3086cbb6a95857d04c2569ccf507
fullstack360 <info@fullstack360.com>
2019-05-19 23:01:58 -0400

    remove commented out code

--- routes/account.js
+++ routes/account.js
@@ -265,18 +265,7 @@ router.get('/:action', function(req, res, next){
 
 router.post('/:action', function(req, res, next){
 	const action = req.params.action
-	// console.log('BODY: ' + JSON.stringify(req.body))
 	const params = req.body
-
-	// if (params.name){ // parse name into firstName and lastName
-	// 	const parts = params.name.split(' ')
-	// 	params['firstName'] = parts[0]
-	// 	if (parts.length > 1)
-	// 		params['lastName'] = parts[parts.length-1]
-	//
-	// 	params['username'] = params.firstName
-	// 	params['confirmed'] = 'yes'
-	// }
 
 	if (action == 'currentuser'){
 		if (params.vertex_session == null){
