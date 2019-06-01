6240371cd5e9e8253c9591662e985c99df78f662
fullstack360 <info@fullstack360.com>
2019-06-01 18:45:42 -0400

    add preview, slug and dateString to events on creation

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -1,5 +1,41 @@
 var Event = require('../models/Event')
+var moment = require('moment')
 var utils = require('../utils')
+
+const slugVersion = (text, numRandomChars) => {
+	let slug = text.toString().toLowerCase()
+			.replace(/\s+/g, '-')           // Replace spaces with -
+			.replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+			.replace(/\-\-+/g, '-')         // Replace multiple - with single -
+			.replace(/^-+/, '')             // Trim - from start of text
+			.replace(/-+$/, '');            // Trim - from end of text
+
+	if (numRandomChars == null)
+		return slug.toLowerCase()
+
+	if (numRandomChars <= 0)
+		return slug.toLowerCase()
+
+	const possible = 'abcdefghijklmnopqrstuvwxyz0123456789'
+	let randomString = ''
+	for (var i=0; i <numRandomChars; i++)
+		randomString += possible.charAt(Math.floor(Math.random() * possible.length))
+
+	return slug.toLowerCase()+'-'+randomString
+}
+
+const scrapePreview = (text, limit) => {
+  $ = cheerio.load(text)
+  let plainText = ''
+  $('p').each((i, element) => {
+    plainText += element.children[0].data
+  })
+
+  if (plainText.length >= limit)
+    return plainText.substring(0, limit)+'...'
+
+  return plainText
+}
 
 module.exports = {
 	get: function(params){
@@ -53,6 +89,23 @@ module.exports = {
 
 	post: function(params){
 		return new Promise(function(resolve, reject){
+
+			// if coming from jQuery AJAX, profile is stringified. Parse it here first.
+			if (params.location != null){
+				try {
+					params['location'] = JSON.parse(params.location)
+				}
+				catch(err){}
+			}
+
+			var date = new Date(params.date)
+			params['dateString'] = moment(date).format('MMMM Do, YYYY')
+			if (params.name != null)
+				params['slug'] = slugVersion(params.name, 6)
+
+			if (params.description != null)
+				params['preview'] = scrapePreview(params.description, 200)
+
 			Event.create(params, function(err, event){
 				if (err){
 					reject(err)
