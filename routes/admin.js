2eee7590a25c394b1823ce5387044e8c83cafd46
fullstack360 <info@fullstack360.com>
2019-06-04 11:39:26 -0400

    endpoint for checking collection data on deployment

--- routes/admin.js
+++ routes/admin.js
@@ -5,28 +5,92 @@ const superagent = require('superagent')
 const router = vertex.router()
 const utils = require('../utils')
 const controllers = require('../controllers')
+var VERTEX_BUCKET = 'turbo360-vertex'
 
-// router.get('/test', (req, res) => {
-// 	const url = 'https://s3.amazonaws.com/turbo360-vertex/pages/landing-test-50-vck340/home.txt'
-// 	return utils.HTTP.get(url)
-// 	.then(config => { // this is a string
-// 		try {
-// 			const pageConfig = JSON.parse(config)
-// 			res.json(pageConfig)
-// 		}
-// 		catch(err) {
-// 			throw err
-// 			return
-// 		}
-// 		return
-// 	})
-// 	.catch(err => {
-// 		res.json({
-// 			confirmation: 'fail',
-// 			message: err.message
-// 		})
-// 	})
-// })
+const createCollection = (collectionName, appSlug) => {
+	console.log('createCollection: ' + collectionName + ', ' + appSlug)
+
+	var params = {
+		bucket: VERTEX_BUCKET,
+		filename: collectionName+'.txt',
+		filetype: 'text/plain',
+		folder: 'stores/'+appSlug
+	}
+
+	utils.AWS.uploadUrl(params, VERTEX_BUCKET)
+	.then(data => {
+		console.log('UPLOAD URL: ' + JSON.stringify(data))
+		return utils.HTTP.put(data.upload, '', {'Content-Type':'text/plain'}) // create empty collection
+	})
+	.then(data => {
+		console.log('Collection Created: ' + JSON.stringify(data))
+	})
+	.catch(err => {
+		console.log('ERR - ' + err)
+	})
+}
+
+const checkCollection = (appSlug, collectionName) => {
+	const url = 'https://s3.amazonaws.com/turbo360-vertex/stores/'+appSlug+'/'+collectionName+'.txt'
+	return utils.HTTP.get(url)
+	.then(config => { // this is a string
+		try {
+			const pageConfig = JSON.parse(config)
+		}
+		catch(err) {
+			throw err
+			return
+		}
+		return
+	})
+	.catch(err => {
+		if (err.status>=400 && err.status<=500){
+			// TODO: create new store file for collection, then upload to bucket.
+			createCollection(collectionName, appSlug)
+			console.log(err.status)
+		}
+	})
+}
+
+// this endpoint checks to see if all the collections for a specified
+// app has the requisite "seed" data on deployment. If not, it
+// seeds the collection with an empty set:
+router.post('/seed', (req, res) => {
+	// const body = {
+	// 	app: 'rocket-man-9gnjll'
+	// }
+
+	if (req.body.app == null){
+		res.json({
+			confirmation: 'fail',
+			message: 'Missing app parameter'
+		})
+		return
+	}
+
+	utils.HTTP.get('https://'+req.body.app+'.vertex360.co/api')
+	.then(response => {
+			const parsed = JSON.parse(response)
+			if (parsed.confirmation != 'success'){
+				throw new Error(parsed.message)
+				return
+			}
+
+			const resources = parsed.data
+			resources.forEach(resource => {
+				if (resource.collectionName) // older versions won't have this
+					checkCollection(req.body.app, resource.collectionName.trim())
+			})
+
+			res.json(parsed)
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+})
 
 router.get('/:slug', (req, res) => {
 	if (req.user == null){
