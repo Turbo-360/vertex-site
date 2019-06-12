7bd23d0fdd4b3f08c54de247dcd032cc0abbfccf
fullstack360 <info@fullstack360.com>
2019-06-12 13:26:55 -0400

    check if site has youtube video associated

--- routes/index.js
+++ routes/index.js
@@ -182,6 +182,10 @@ router.get('/templates', (req, res) => {
 			site['index'] = i
 			site['tags'] = site.tags.slice(0, 3) // use only first 3
 			site['description'] = utils.TextUtils.convertToHtml(site.description)
+
+			site['hasVideo'] = false
+			if (site.template.video != null)
+				site['hasVideo'] = (site.template.video.length==11) // youtube IDs are 11 characters
 		})
 
 		data['templates'] = sites
