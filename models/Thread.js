d02fa8e4a4b8a3241928ed76398ac2731c8bf2a5
fullstack360 <info@fullstack360.com>
2019-07-05 23:48:07 -0400

    updated schema for thread

--- models/Thread.js
+++ models/Thread.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
 
 const ThreadSchema = new mongoose.Schema({
-	author: {type:mongoose.Schema.Types.Mixed, default:{}},
-  post: {type:mongoose.Schema.Types.Mixed, default:{}},
+  subject: {type:mongoose.Schema.Types.Mixed, default:{}}, // post, site, profile, etc
 	slug: {type:String, lowercase:true, trim:true, default:''},
-	link: {type:String, trim:true, default:''}, // for links to outside posts
+	// link: {type:String, trim:true, default:''}, // for links to outside posts
 	numReplies: {type:Number, default:0},
 	votes: {type:mongoose.Schema.Types.Mixed, default:{up:[], down:[], score:0}},
 	dateString: {type:String, default:'' },
@@ -13,19 +12,10 @@ const ThreadSchema = new mongoose.Schema({
 
 ThreadSchema.methods.summary = function(authLevel) {
 	var summary = {
-		title: this.title,
-		tags: this.tags,
-		image: this.image,
-		author: this.author,
-		preview: this.preview,
-		text: this.text,
+		subject: this.subject,
 		slug: this.slug,
-		isPublic: this.isPublic,
 		numReplies: this.numReplies,
-		link: this.link,
-		type: this.type,
-		votes: {up:this.votes.up.length, down:this.votes.down.length, score:this.votes.score},
-		thread: this.thread,
+		votes: this.votes,
 		dateString: this.dateString,
 		timestamp: this.timestamp,
 		schema: 'thread',
