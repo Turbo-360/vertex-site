395f8f409927f3552fe0a7c4845dfbbc3d95a5ba
fullstack360 <info@fullstack360.com>
2019-07-28 15:09:45 -0400

    misc

--- medium/routes/index.js
+++ medium/routes/index.js
@@ -151,16 +151,16 @@ router.get('/templates', (req, res) => {
 	.then(sites => {
 		const templatesMap = {}
 		sites.forEach((site, i) => {
-			site['index'] = i
 			site['features'] = site.tags.join(' ').toLowerCase()
-			site['tags'] = site.tags.slice(0, 3) // use only first 3
 			site['description'] = utils.TextUtils.convertToHtml(site.description)
-
-			site['hasVideo'] = false
-			if (site.template.video != null)
-				site['hasVideo'] = (site.template.video.length==11) // youtube IDs are 11 characters
-
 			templatesMap[site.id] = site
+
+			// site['index'] = i
+			// site['tags'] = site.tags.slice(0, 3) // use only first 3
+
+			// site['hasVideo'] = false
+			// if (site.template.video != null)
+			// 	site['hasVideo'] = (site.template.video.length==11) // youtube IDs are 11 characters
 		})
 
 		data['templates'] = sites
