194e0e637398b987473dbd073a7fe02c6b02776c
fullstack360 <info@fullstack360.com>
2019-08-04 19:16:10 -0400

    paypal processing route

---
+++ routes/paypal.js
@@ -0,0 +1,63 @@
+// Full Documentation - https://www.turbo360.co/docs
+// const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+// const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+// const router = vertex.router()
+
+const express = require('express')
+const router = express.Router()
+const checkoutNodeJssdk = require('@paypal/checkout-server-sdk')
+const payPalClient = require('../utils/PayPalClient')
+
+// https://developer.paypal.com/docs/checkout/integrate/#6-verify-the-transaction
+router.post('/order', (req, res) => {
+  const itemID = req.body.item
+  const siteID = req.body.site
+  const orderID = req.body.orderID
+
+  let selectedItem = null
+  controllers.site.getById(siteID, null, 'admin')
+  .then(site => {
+    if (site.paypal.clientId.length == 0){
+      throw new Error('Missing PayPal Client ID')
+      return
+    }
+
+    if (site.paypal.clientSecret.length == 0){
+      throw new Error('Missing PayPal Client Secret')
+      return
+    }
+
+    return controllers.item.getById(itemID)
+  })
+  .then(item => {
+    selectedItem = item
+    let request = new checkoutNodeJssdk.orders.OrdersGetRequest(orderID)
+    const credentials = {
+      clientId: site.paypal.clientId,
+      clientSecret: site.paypal.clientSecret
+    }
+
+    return payPalClient.client(credentials).execute(request)
+  })
+  .then(data => {
+    // Validate the transaction details are as expected
+    // if (data.result.purchase_units[0].amount.value !== '3.00') {
+    //   throw new Error('Incorrect Amount.')
+    //   return
+    // }
+
+    // console.log('ORDER SUCCESSFULLY PROCESSED: ' + JSON.stringify(data))
+    res.json({
+      confirmation: 'success',
+      data: data.result
+    })
+  })
+  .catch(err => {
+    res.json({
+      confirmation: 'fail',
+      message: err.message
+    })
+  })
+})
+
+module.exports = router
