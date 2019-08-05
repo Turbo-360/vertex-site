7f792fde01f57c49e6b14c856d4a5f54ad52e17e
fullstack360 <info@fullstack360.com>
2019-08-04 22:19:12 -0400

    add type to item

--- models/Item.js
+++ models/Item.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose')
 var Item = new mongoose.Schema({
 	site: {type:mongoose.Schema.Types.Mixed, default:{}},
 	name: {type:String, trim:true, default:''},
+	type: {type:String, trim:true, default:'physical'}, // physical or digital. if pysical, require mailing address
   price: {type:Number, default:0},
   description: {type:String, trim:true, default: ''},
   image: {type:String, trim:true, default: process.env.IMAGE_PLACEHOLDER},
@@ -14,6 +15,7 @@ Item.methods.summary = function() {
 	return {
 		site: this.site,
 		name: this.name,
+		type: this.type,
     price: this.price,
     description: this.description,
 		image: (this.image.length == 0) ? process.env.IMAGE_PLACEHOLDER : this.image,
