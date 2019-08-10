f2621b9b145aa16446a144aeaaddc500af5035ab
fullstack360 <info@fullstack360.com>
2019-08-10 16:57:19 -0400

    updated app schema

--- models/App.js
+++ models/App.js
@@ -1,10 +1,15 @@
 var mongoose = require('mongoose')
 
 var App = new mongoose.Schema({
-	name: {type:String, trim:true, default:''},
+	name: {type:String, trim:true, lowercase:true, default:''},
   profile: {type:mongoose.Schema.Types.Mixed, default:{}},
   image: {type:String, trim:true, default:''},
   description: {type:String, trim:true, default:''},
+	preview: {type:String, trim:true, default: ''},
+	tags: {type:Array, default:[]},
+	slug: {type:String, lowercase:true, trim:true, default:''},
+	link: {type:String, trim:true, default:''}, // link to 3rd party landing page, if any
+	votes: {type:mongoose.Schema.Types.Mixed, default:{up:[], down:[], score:0}},
 	timestamp: {type:Date, default:Date.now}
 })
 
@@ -14,6 +19,11 @@ App.methods.summary = function() {
     profile: this.profile,
     image: this.image,
     description: this.description,
+		preview: this.preview,
+		tags: this.tags,
+		slug: this.slug,
+		link: this.link,
+		votes: this.votes,
 		timestamp: this.timestamp,
 		schema: 'app',
 		id: this._id.toString()
